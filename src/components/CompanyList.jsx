68dddd506770f69c35ee69a57bda8509cb209def
amanbragta <amanbragta@gmail.com>
2025-05-19 17:02:22 +0530

    added loading state

--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
 function CompanyList() {
-    const {data}= useQuery({
+    const {data, isLoading}= useQuery({
         queryKey:['postings'],
         queryFn:()=>{
             return axios.get(`${import.meta.env.VITE_API_URL}/jobs/postings`)
@@ -12,29 +12,14 @@ function CompanyList() {
     })
   return (
     <div className='mt-[50px] mx-[64px] grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-center'>
-        {data?.data.map(item=>(
+        {isLoading ? <h1 className='text-center text-2xl font-medium'>Loading...</h1>:<>
+             {data?.data.map(item=>(
              <div key={item._id} className='flex-[0_1_calc(25%-8px)] min-w-[290px] max-w-[316px]'>
              <Card details={item}/>
          </div>
         ))}
-        {/* <div className='flex-[0_1_calc(25%-8px)] min-w-[290px] max-w-[316px]'>
-            <Card/>
-        </div>
-        <div className='flex-[0_1_calc(25%-8px)] min-w-[290px] max-w-[316px]'>
-            <Card/>
-        </div>
-        <div className='flex-[0_1_calc(25%-8px)] min-w-[290px] max-w-[316px]'>
-            <Card/>
-        </div>
-        <div className='flex-[0_1_calc(25%-8px)] min-w-[290px] max-w-[316px]'>
-            <Card/>
-        </div>
-        <div className='flex-[0_1_calc(25%-8px)] min-w-[290px] max-w-[316px]'>
-            <Card/>
-        </div>
-        <div className='flex-[0_1_calc(25%-8px)] min-w-[290px] max-w-[316px]'>
-            <Card/>
-        </div> */}
+        </>
+        }
     </div>
   )
 }
